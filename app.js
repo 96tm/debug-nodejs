6abf8b4365b1b6b9dd153ce2672077f098c00dc9
96tm <khabcity@gmail.com>
2021-05-22 11:49:55 +0300

    refactor: removed unused bodyParser from app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const db = require('./db');
 const user = require('./controllers/user-controller');
 const game = require('./controllers/game-controller');
-const bodyParser = require('body-parser');
 
 db.sync();
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'));
 app.use('/api/game', game);
-app.listen(4000, function() {
-    console.log("App is listening on 4000");
+app.listen(4000, function () {
+  console.log('App is listening on 4000');
 });
