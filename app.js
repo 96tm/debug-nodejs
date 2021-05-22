ca4518d254584ee80d6d3177459ee4050137406b
96tm <khabcity@gmail.com>
2021-05-22 11:27:05 +0300

    fix: specify express server port and postgres port, fix body-parser import in app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var app = express();
 var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
+const bodyParser = require('body-parser');
 
 db.sync();
-app.use(require('body-parser'));
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function() {
     console.log("App is listening on 4000");
 })
--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const Sequelize = require('sequelize');
                                 //database username   password
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: '5433'
 });
 
 sequelize.authenticate().then(
