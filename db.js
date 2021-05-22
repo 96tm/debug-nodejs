0d4ac5ec0487732c5a43d5374bd8cdaab2183bc9
96tm <khabcity@gmail.com>
2021-05-22 11:35:53 +0300

    refactor: use arrow functions in sequilize.authenticate and catch clause in db.js

--- db.js
+++ db.js
@@ -1,21 +1,14 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-                                //database username   password
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
     port: '5433'
 });
 
-sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
-
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-);
+sequelize.authenticate()
+         .then(() => console.log("Connected to DB"))
+         .catch((err) =>console.log(`Error: ${err}`));
 
 module.exports = sequelize;
