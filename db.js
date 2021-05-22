feb8b5b862fc3a7b610eae1e5418d852534fa5d8
96tm <khabcity@gmail.com>
2021-05-22 11:43:53 +0300

    refactor: use 2-space indentation in db.js

--- db.js
+++ db.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
     host: 'localhost',
     dialect: 'postgres',
-    port: '5433'
-});
+    port: '5433',
+  }
+);
 
-sequelize.authenticate()
-         .then(() => console.log("Connected to DB"))
-         .catch((err) =>console.log(`Error: ${err}`));
+sequelize
+  .authenticate()
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log(`Error: ${err}`));
 
 module.exports = sequelize;
